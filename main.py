4659b1ab77389552b22cf8004bb223ade5ba343e
WladykaPawel <wladykapa@gmail.com>
2022-08-16 16:16:17 +0200

    Update no.3

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 from tkinter import filedialog
 from fpdf import FPDF
 from datetime import date
-import tkinter as tk
+import tkinter as Apka_do_zamówień
 import PIL.Image
 from PIL import ImageTk
 import os
@@ -16,113 +16,128 @@ from tkinter import *
 fdate = date.today().strftime('%d.%m.%Y')
 
 
-
-
 # ============================APLICATION==========================================
-root = tk.Tk()
+root = Apka_do_zamówień.Tk()
+root.title("Aplikacja do zamównień publicznych")
+root.iconbitmap("favicon.ico")
 
-canvas1 = tk.Canvas(root, width=1400, height=900)
+
+canvas1 = Apka_do_zamówień.Canvas(root, width=1400, height=900)
+
+logo = PIL.Image.open("logo.png")
+logo_resized = logo.resize((50, 50))  # new width & height
+logo = ImageTk.PhotoImage(logo_resized)
+Logo = Apka_do_zamówień.Label(text='Upload File', image=logo)
+canvas1.create_window(1200, 50, window=Logo)
+
+title = Apka_do_zamówień.Label(root, text='LEGENDARY ROVER')
+title.config(font=('helvetica', 15))
+canvas1.create_window(1200, 90, window=title)
+
+title = Apka_do_zamówień.Label(root, text='   TEAM   ')
+title.config(font=('helvetica', 15))
+canvas1.create_window(1200, 120, window=title)
 
 
-label1 = tk.Label(root, text='Opiekun:')
+label1 = Apka_do_zamówień.Label(root, text='Opiekun:')
 label1.config(font=('helvetica', 10))
 canvas1.create_window(50, 20, window=label1)
 
-entry1 = tk.Entry(root, width=40)
+entry1 = Apka_do_zamówień.Entry(root, width=40)
 entry1.place(x=10, y=440, width=40, height=20000)
 canvas1.create_window(150, 40, window=entry1)
 
-label2 = tk.Label(root, text='Katedra:')
+label2 = Apka_do_zamówień.Label(root, text='Katedra:')
 label2.config(font=('helvetica', 10))
 canvas1.create_window(400, 20, window=label2)
 
-entry2 = tk.Entry(root, width=40)
+entry2 = Apka_do_zamówień.Entry(root, width=40)
 canvas1.create_window(500, 40, window=entry2)
 
-label3 = tk.Label(root, text='Specyfikacja produktu (opis):')
+label3 = Apka_do_zamówień.Label(root, text='Specyfikacja produktu (opis):')
 label3.config(font=('helvetica', 10))
 canvas1.create_window(110, 80, window=label3)
 
-entry3 = tk.Entry(root, width=180)
-canvas1.create_window(570, 100, window=entry3)
+entry3 = Apka_do_zamówień.Entry(root, width=150)
+canvas1.create_window(480, 100, window=entry3)
 
 
-Title1 = tk.Label(root, text='Pierwszy Produkt:')
+Title1 = Apka_do_zamówień.Label(root, text='Pierwszy Produkt:')
 Title1.config(font=('helvetica', 15))
 canvas1.create_window(600, 140, window=Title1)
 
 
 
-label4 = tk.Label(root, text='Nazwa oferty:')
+label4 = Apka_do_zamówień.Label(root, text='Nazwa oferty:')
 label4.config(font=('helvetica', 10))
 canvas1.create_window(65, 180, window=label4)
-entry4 = tk.Entry(root, width=70)
+entry4 = Apka_do_zamówień.Entry(root, width=70)
 canvas1.create_window(240, 200, window=entry4)
 
-label5 = tk.Label(root, text='Link:')
+label5 = Apka_do_zamówień.Label(root, text='Link:')
 label5.config(font=('helvetica', 10))
 canvas1.create_window(45, 240, window=label5)
-entry5 = tk.Entry(root, width=70)
+entry5 = Apka_do_zamówień.Entry(root, width=70)
 canvas1.create_window(240, 260, window=entry5)
 
-label6 = tk.Label(root, text='Cena:')
+label6 = Apka_do_zamówień.Label(root, text='Cena:')
 label6.config(font=('helvetica', 10))
 canvas1.create_window(45, 300, window=label6)
-entry6 = tk.Entry(root, width=70)
+entry6 = Apka_do_zamówień.Entry(root, width=70)
 canvas1.create_window(240, 320, window=entry6)
 
 
 
-button1 = tk.Button(text='Upload File', width=20, height=2, command = lambda:upload_file())
+button1 = Apka_do_zamówień.Button(text='Upload File', width=20, height=2, command = lambda:upload_file())
 canvas1.create_window(1000, 220, window=button1)
 
 
 
-Title2 = tk.Label(root, text='Drugi Produkt:')
+Title2 = Apka_do_zamówień.Label(root, text='Drugi Produkt:')
 Title2.config(font=('helvetica', 15))
 canvas1.create_window(600, 460, window=Title2)
 
 
 
-label7 = tk.Label(root, text='Nazwa kontroferty:')
+label7 = Apka_do_zamówień.Label(root, text='Nazwa kontroferty:')
 label7.config(font=('helvetica', 10))
-canvas1.create_window(65, 500, window=label7)
-entry7 = tk.Entry(root, width=70)
+canvas1.create_window(85, 500, window=label7)
+entry7 = Apka_do_zamówień.Entry(root, width=70)
 canvas1.create_window(240, 520, window=entry7)
 
-label8 = tk.Label(root, text='Link:')
+label8 = Apka_do_zamówień.Label(root, text='Link:')
 label8.config(font=('helvetica', 10))
 canvas1.create_window(45, 560, window=label8)
-entry8 = tk.Entry(root, width=70)
+entry8 = Apka_do_zamówień.Entry(root, width=70)
 canvas1.create_window(240, 580, window=entry8)
 
-label9 = tk.Label(root, text='Cena:')
+label9 = Apka_do_zamówień.Label(root, text='Cena:')
 label9.config(font=('helvetica', 10))
 canvas1.create_window(45, 620, window=label9)
-entry9 = tk.Entry(root, width=70)
+entry9 = Apka_do_zamówień.Entry(root, width=70)
 canvas1.create_window(240, 640, window=entry9)
 
 
-button2 = tk.Button(text='Upload File', width=20, height=2, command = lambda:upload_file2())
+button2 = Apka_do_zamówień.Button(text='Upload File', width=20, height=2, command = lambda:upload_file2())
 canvas1.create_window(1000, 520, window=button2)
 
-label10 = tk.Label(root, text='Ostateczny wybór oferty:')
+label10 = Apka_do_zamówień.Label(root, text='Ostateczny wybór oferty:')
 label10.config(font=('helvetica', 10))
 canvas1.create_window(100, 670, window=label10)
-entry10 = tk.Entry(root, width=70)
-canvas1.create_window(440, 670, window=entry10)
+entry10 = Apka_do_zamówień.Entry(root, width=70)
+canvas1.create_window(400, 670, window=entry10)
 
-label11 = tk.Label(root, text='Przeznaczone środki:')
+label11 = Apka_do_zamówień.Label(root, text='Przeznaczone środki:')
 label11.config(font=('helvetica', 10))
-canvas1.create_window(100, 700, window=label11)
-entry11 = tk.Entry(root, width=70)
-canvas1.create_window(440, 700, window=entry11)
+canvas1.create_window(92, 700, window=label11)
+entry11 = Apka_do_zamówień.Entry(root, width=70)
+canvas1.create_window(400, 700, window=entry11)
 
-label12 = tk.Label(root, text='Źródło finansowania:')
+label12 = Apka_do_zamówień.Label(root, text='Źródło finansowania:')
 label12.config(font=('helvetica', 10))
-canvas1.create_window(100, 730, window=label12)
-entry12 = tk.Entry(root, width=70)
-canvas1.create_window(440, 730, window=entry12)
+canvas1.create_window(88, 730, window=label12)
+entry12 = Apka_do_zamówień.Entry(root, width=70)
+canvas1.create_window(400, 730, window=entry12)
 
 def upload_file():
     if os.path.exists("img1.png"):
@@ -135,7 +150,7 @@ def upload_file():
     Pimg = PIL.Image.open(filename)
     Pimg_resized=Pimg.resize((280,200)) # new width & height
     Pimg=ImageTk.PhotoImage(Pimg_resized)
-    picture1 = tk.Label(text='Upload File', image=Pimg)
+    picture1 = Apka_do_zamówień.Label(text='Upload File', image=Pimg)
     canvas1.create_window(1000, 280, window=picture1)
 
 def upload_file2():
@@ -149,7 +164,7 @@ def upload_file2():
     Pimg2 = PIL.Image.open(filename)
     Pimg2_resized=Pimg2.resize((280,200)) # new width & height
     Pimg2=ImageTk.PhotoImage(Pimg2_resized)
-    picture2 = tk.Label(text='Upload File', image=Pimg2)
+    picture2 = Apka_do_zamówień.Label(text='Upload File', image=Pimg2)
     canvas1.create_window(1000, 580, window=picture2)
 canvas1.pack()
 
@@ -248,7 +263,7 @@ def getSquareRoot():
     pdf.output("Zamównie Publiczne.pdf")
 
 
-buttonend = tk.Button(text='Stwórz PDF-a', command=getSquareRoot, width=20, height=2)
+buttonend = Apka_do_zamówień.Button(text='Stwórz PDF-a', command=getSquareRoot, width=20, height=2)
 canvas1.create_window(600, 780, window=buttonend)
 
 root.mainloop()
